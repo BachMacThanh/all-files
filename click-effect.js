d5ef0c723f21c5cfbd92a42daa0642bfbf1dd7a1
DangDev <dream858vip@gmail.com>
2021-06-11 13:43:02 +0700

    Update click-effect.js

--- click-effect.js
+++ click-effect.js
@@ -1,96 +1,42 @@
 "use strict";
 
-const COLORS = {
-  RED: '#FD5061',
-  YELLOW: '#FFCEA5',
-  BLACK: '#29363B',
-  WHITE: 'white',
-  VINOUS: '#A50710'
-};
-const burst1 = new mojs.Burst({
-  left: 0,
-  top: 0,
-  count: 8,
-  radius: {
-    50: 150
+const OPTS = {
+  fill: 'none',
+  radius: 25,
+  strokeWidth: {
+    50: 0
   },
-  children: {
-    shape: 'line',
-    stroke: ['white', '#FFE217', '#FC46AD', '#D0D202', '#B8E986', '#D0D202'],
-    scale: 1,
-    scaleX: {
-      1: 0
-    },
-    // pathScale:    'rand(.5, 1.25)',
-    degreeShift: 'rand(-90, 90)',
-    radius: 'rand(20, 40)',
-    // duration:     200,
-    delay: 'rand(0, 150)',
-    isForce3d: true
-  }
-});
-const largeBurst = new mojs.Burst({
-  left: 0,
-  top: 0,
-  count: 4,
-  radius: 0,
-  angle: 45,
-  radius: {
-    0: 450
-  },
-  children: {
-    shape: 'line',
-    stroke: '#4ACAD9',
-    scale: 1,
-    scaleX: {
-      1: 0
-    },
-    radius: 100,
-    duration: 450,
-    isForce3d: true,
-    easing: 'cubic.inout'
-  }
-});
-const CIRCLE_OPTS = {
-  left: 0,
-  top: 0,
   scale: {
     0: 1
-  }
+  },
+  angle: {
+    'rand(-35, -70)': 0
+  },
+  duration: 500,
+  left: 0,
+  top: 0,
+  easing: 'cubic.out'
 };
-const largeCircle = new mojs.Shape({ ...CIRCLE_OPTS,
-  fill: 'none',
-  stroke: 'white',
-  strokeWidth: 4,
-  opacity: {
-    .25: 0
-  },
-  radius: 250,
-  duration: 600
+const circle1 = new mojs.Shape({ ...OPTS,
+  stroke: 'cyan'
 });
-const smallCircle = new mojs.Shape({ ...CIRCLE_OPTS,
-  fill: 'white',
-  opacity: {
-    .5: 0
+const circle2 = new mojs.Shape({ ...OPTS,
+  radius: {
+    0: 15
   },
-  radius: 30
+  strokeWidth: {
+    30: 0
+  },
+  stroke: 'magenta',
+  delay: 'rand(75, 150)'
 });
 document.addEventListener('click', function (e) {
-  burst1.tune({
-    x: e.pageX,
-    y: e.pageY
-  }).generate().replay();
-  largeBurst.tune({
+  circle1.tune({
     x: e.pageX,
     y: e.pageY
   }).replay();
-  largeCircle.tune({
-    x: e.pageX,
-    y: e.pageY
-  }).replay();
-  smallCircle.tune({
+  circle2.tune({
     x: e.pageX,
     y: e.pageY
   }).replay();
 });
-
